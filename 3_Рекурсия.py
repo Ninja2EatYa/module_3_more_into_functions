d5de1083c7702ff2f2168d2809462c5545fc8fb9
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-06-06 18:12:16 +0300

    Update 3_Рекурсия.py

--- 3_Рекурсия.py
+++ 3_Рекурсия.py
@@ -7,4 +7,4 @@ def get_multiplied_digits(number):
         return first * get_multiplied_digits(int(str_number[1:]))
 
 number = input('Введите любое число: ',)
-print('Ответ: ', get_multiplied_digits(number))
+print('Произведение всех цифр числа: ', get_multiplied_digits(number))
