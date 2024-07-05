42cfe82e609ad682d35dfe0f417fcbf990372b65
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-07-05 16:45:01 +0300

    Update 1_Однокоренные.py

--- 1_Однокоренные.py
+++ 1_Однокоренные.py
@@ -2,12 +2,11 @@ def single_root_words(root_word, *other_words):
     same_words = []
     root_word = str(root_word).lower()
     for word in other_words:
-        word = str(word).lower()
-        if word in root_word:
+        if word.lower() in root_word.lower():
             same_words.append(word)
-        elif root_word in word:
+        elif root_word.lower() in word.lower():
             same_words.append(word)
-    print(same_words)
+    return same_words
 
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
